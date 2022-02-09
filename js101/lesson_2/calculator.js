6b6625783d84ca84885b164edc33d6b26de08a8b
Herschel Schwartz <herschelrschwartz@gmail.com>
2022-02-09 12:10:48 -0800

    more exercises

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -3,14 +3,32 @@ function prompt(message) {
   console.log(`==> ${message}`);
 }
 
+function invalidNumber(num) {
+  return isNaN(Number(num)) || num.trimStart() === "";
+}
+
 prompt("Welcome to the calculator.");
 prompt("What is the first number?");
-let num1 = Number(rlSync.prompt());
+let num1 = rlSync.question();
+while (invalidNumber(num1)) {
+  prompt("That doesn't seem to be a valid number. Please enter another.");
+  num1 = rlSync.question();
+}
 prompt("What is the second number?");
-let num2 = Number(rlSync.prompt());
-prompt("What operation would you like to perform?\n1. Addition 2. Subtraction 3. Multiplication 4. Division")
-let operation = rlSync.prompt();
+let num2 = rlSync.question();
+while (invalidNumber(num2)) {
+  prompt("That doesn't seem to be a valid number. Please enter another.");
+  num2 = rlSync.question();
+}
+prompt("What operation would you like to perform?\n1. Addition 2. Subtraction 3. Multiplication 4. Division");
+let operation = rlSync.question();
+while (!["1", "2", "3", "4"].includes(operation)) {
+  prompt("Must choose 1, 2, 3, or 4.");
+  operation = rlSync.question();
+}
 let output;
+num1 = Number(num1);
+num2 = Number(num2);
 switch (operation) {
   case "1":
     output = num1 + num2;
